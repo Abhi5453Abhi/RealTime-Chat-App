43ea2a7a32ecf667aa2ea9431359d85ee26b3949
Admin <admin@Admins-MacBook-Air.local>
2020-11-27 20:04:44 +0530

    Version 1.0

---
+++ nodeServer/index.js
@@ -0,0 +1,21 @@
+//This server will handle socket io
+const io = require('socket.io')(4000)
+
+const users = {};
+
+io.on('connection',socket => {
+
+   socket.on('new-user-joined',name => {
+      users[socket.id] = name;
+      socket.broadcast.emit('user-joined',name);
+   });
+
+   socket.on('send',message => {
+      socket.broadcast.emit('recieve',{message:message, name:users[socket.id]})
+   });
+
+   socket.on('disconnect',message => {
+      socket.broadcast.emit('left',{message:message, name:users[socket.id]});
+      delete users[socket.id];
+   })
+})
